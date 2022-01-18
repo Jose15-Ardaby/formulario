3f9dc2ac7849f08b9c89771708ad525c9d8b8985
Jose15-Ardaby <96440341+Jose15-Ardaby@users.noreply.github.com>
2022-01-18 11:59:36 -0600

    Add files via upload

--- conexion.php
+++ conexion.php
@@ -1,16 +1,9 @@
  <?php
-class Cconexion{
-    function ConexionBD(){
-$contraseña = "Primus90.$@";
-$usuario = "sa";
-$nombreBaseDeDatos = "CRM";
-# Puede ser 127.0.0.1 o el nombre de tu equipo; o la IP de un servidor remoto
-$rutaServidor = "qa.ardabytec.vip";
-try {
-    $conn = new PDO("sqlsrv:server=$rutaServidor;database=$nombreBaseDeDatos", $usuario, $contraseña);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "<h1>Success</h1>";
-} catch (Exception $e) {
-    echo "Ocurrió un error con la base de datos: " . $e->getMessage();
-}}}
+
+$conexion=new PDO("sqlsrv:server=10.170.12.113;database=CRM","sa","Primus90.$@");
+$consulta=$conexion->prepare("Select * from tb_hubspot_tw");
+$consulta->execute();
+$datos=$consulta->fetchAll(PDO::FETCH_ASSOC);
+var_dump($datos);
+
 ?>
